72edb6a6113567aca487017fd02dade2a07d6afb
Jack Brennan <jack.brennan@nvngsystems.com>
2019-05-28 10:59:11 -0600

    Updated index.js to refactor to a method

---
+++ index_bkp2.js
@@ -0,0 +1,36 @@
+//This is bkp2 before factoring using methods  - keep as a reference
+
+// Create a new task by adding to the arrays
+// A new task will be created as incomplete
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false
+  };
+  return task;
+}
+
+// prints out the provided task's details
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+}
+
+// marks the provided task as completed
+function completeTask(task) {
+  task.complete = true;
+}
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
+
+// for now, let's just make sure we see our tasks
+console.log(tasks);
+
